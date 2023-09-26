ac5758a28bffb983b7dcc21890b0a9d1947ecea4
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-09-26 16:35:18 -0300

    Finalizando projeto

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,29 @@
+import style from './Lista.module.scss'
+import Item from './Item'
+import { ITarefa } from '../../types/tarefa'
+
+interface Props{
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
+
+export default function Lista({tarefas, selecionaTarefa}: Props) {
+    
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {tarefas.map((item) => (
+                    <Item 
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
+                        // tarefa={item.tarefa}
+                        // tempo={item.tempo}
+                        //ou
+                        {...item}
+                    />
+                ))}
+            </ul>
+        </aside>
+    )
+}
